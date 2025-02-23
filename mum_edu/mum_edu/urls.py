59726192410704c05bb12366f83472f21a32d8f3
Ualikhan21 <uali.zhunisbek@gmail.com>
2025-02-23 22:58:00 +0500

    Initial commit

--- mum_edu/mum_edu/urls.py
+++ mum_edu/mum_edu/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),  # Главная страница
 ]
---
+++ mum_edu/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ["./templates/**/*.html", "./static/**/*.js"], // Пути к шаблонам и JS-файлам
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
